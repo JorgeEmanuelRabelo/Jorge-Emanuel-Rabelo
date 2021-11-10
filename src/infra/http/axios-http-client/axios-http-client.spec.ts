2c6b38eb0ad88035aacea305fe5de54f8c737e15
JorgeRabelo <jorgerabelo17@gmail.com>
2021-11-10 10:22:15 -0300

    test: implementado AxiosHttpClient com valor de retorno correto

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,6 +5,11 @@ import { HttpPostClientParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockResponse = {
+  data: faker.random.objectElement(),
+  status: faker.random.number()
+}
+mockedAxios.post.mockResolvedValue(mockResponse)
 
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
@@ -21,5 +26,15 @@ describe('AxiosHttpClient', () => {
     const sut = makeSut()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  test('Testando valores de retorno statusCode e body', async () => {
+    const sut = makeSut()
+    const httpResponse = await sut.post(mockPostResquest())
+    expect(httpResponse).toEqual(
+      {
+        statusCode: mockResponse.status,
+        body: mockResponse.data
+      })
   })
 })
