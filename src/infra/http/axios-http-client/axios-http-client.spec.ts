1503984d725c49f495620cf39cb52ff78bac5dab
JorgeRabelo <jorgerabelo17@gmail.com>
2021-11-10 09:57:29 -0300

    test: adicionado AxiosHttpClient com verbo correto

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -10,10 +10,10 @@ const makeSut = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('Testando URL correta', async () => {
+  test('Testando URL correta e verbo', async () => {
     const url = faker.internet.url()
     const sut = makeSut()
     await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
